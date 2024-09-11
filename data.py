e407ab4579e98dc93155b2926490cb3959adf547
inking02 <ludomartvicotec@gmail.com>
2024-09-11 14:51:10 -0400

    ajout du fichier contenant les données du pulsar

---
+++ data.py
@@ -0,0 +1,15 @@
+from ucimlrepo import fetch_ucirepo 
+import numpy as np
+  
+# fetch dataset 
+htru2 = fetch_ucirepo(id=372) 
+  
+# data (as pandas dataframes) 
+X = htru2.data.features 
+y = htru2.data.targets 
+  
+# metadata 
+print(X) 
+  
+# variable information 
+print(y)
