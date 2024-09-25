6935b9389598e2d278a65c0147ff52486bc0e6e0
yannis menard <yannismenard@icloud.com>
2024-09-25 14:51:25 -0400

    Working version
    Optimisation to do in the future possibly but it works fine

--- data.py
+++ data.py
@@ -1,10 +1,5 @@
-import numpy as np
 import pandas as pd
 import random
-import os
-
-print(os.getcwd()) 
-
 
 def get_data_file():
 
@@ -16,34 +11,19 @@ def get_data_file():
 def get_samples(data, sample_amount):
 
     sample_array = []
-    zero_value = 0
-    one_value = 0
+    zero_value = 1
+    one_value = 1
     single_data = []
 
-    # for i in range(len(data)):
-    #     if data[i][8] == "0" or zero_value >= (sample_amount/2):
-    #         sample_array += data[i]
-
-    #     elif data[i][8] == "1" or one_value >= (sample_amount/2):
-    #         sample_array += data[i]
-
-    while len(sample_array) <= sample_amount:
+    while len(sample_array) < sample_amount:
         single_data = random.choice(data)
-
-        if single_data[8] == "0" and zero_value >= (sample_amount/2):
-            sample_array += single_data
+        
+        if single_data[8] == 0.0 and zero_value <= (sample_amount/2):
+            sample_array.append(single_data)
             zero_value += 1
 
-        elif single_data[8] == "1" and one_value >= (sample_amount/2):
-            sample_array += single_data
+        elif single_data[8] == 1.0 and one_value <= (sample_amount/2):
+            sample_array.append(single_data)
             one_value += 1
-
+        print(len(sample_array))
     return sample_array
-
-#data = np.genfromtxt('HTRU_2.csv', delimiter=',', skip_header=0)
-
-data = get_data_file()
-
-#a = get_samples(data, 20)
-
-#print(a)
