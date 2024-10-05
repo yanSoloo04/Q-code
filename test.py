a4075235b7f30dbdd968caa2850f63150fd5fccf
yannis menard <yannismenard@icloud.com>
2024-10-04 21:43:56 -0400

    Test avec les dry beannnns qui fonctionne

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import data
 
 a = data.get_csv_file('HTRU_2.csv')
 b, c = data.get_samples(a, 10, [0.0, 1.0])
-d = 'Dermosan'
-e = 'Sira'
-print(c)
+d = ['DERMASON', 'SIRA']
+e = data.get_xlsx_file('Dry_Bean_Dataset.xlsx')
+f, g = data.get_samples(e, 10, d)
+print(f)
