bd59ce6f5cacf7c460893e8bd52b64fa52f0f132
inking02 <ludomartvicotec@gmail.com>
2024-10-17 12:30:07 -0400

    MLPC est en forme finale!!!

--- MLPC.py
+++ MLPC.py
@@ -7,16 +7,17 @@ from sklearn.preprocessing import StandardScaler
 from data import get_csv_file, get_samples, get_xlsx_file
 
 
-def run_MLPC(parameters:NDArray, labels:NDArray, matrix:bool )->int:
+def run_MLPC(parameters:NDArray, labels:NDArray, matrix:bool = False, filename:str = '')->float:
     """
     Runs the mlpc with the given dataset and returns the accuracy score of the classification using the mlpc method.
     Args:
     parameters (NDArray): the parameters for each given labels in the form of a 2D numpy array
     labels (NDArray): the labels corresponding to the parameters in the form of a 1D numpy array
     matrix (bool): the users specifies wheter he wants to print the confusion matrix or not.
+    filename (str): the name of the file for the confusion matrix to be saved as
 
     Returns:
-    the accuracy score after using the MLPC method to classify the datas.
+    the accuracy score after using the MLPC method to classify the datas in the form of a float object.
     """
     #scaling the datas and splitting them into testing datas and training datas.
     scaler = StandardScaler().fit(parameters)
@@ -35,23 +36,31 @@ def run_MLPC(parameters:NDArray, labels:NDArray, matrix:bool )->int:
     print('Actual labels: ', y_test)
     print('Predicted labels: ', predictions)
 
+    #saving the confusion matrix if the user wants to
     if matrix == True:
         cm = confusion_matrix(y_test, predictions)
-        draw_confusion_matrix(cm, "Confusion Matrix")
+        draw_confusion_matrix(cm, filename)
     return acc
 
-def draw_confusion_matrix(matrix: NDArray, title: str):
+def draw_confusion_matrix(matrix: NDArray, file_name:str):
+    """
+    This functions draws the confusion matrix and saves it as the filename on the same folder as the python script
+    Args:
+    matrix (NDArray): the array containing the confusion matrix to print
+    file_name (str): the name of the file for the confusion matrix to be saved as
+    """
     plt.figure(figsize=(6, 6))
     plt.imshow(matrix, interpolation='nearest', cmap='coolwarm', aspect='equal')
-    plt.title(title)
+    plt.title( "Confusion Matrix")
     plt.colorbar()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.xticks([0, 1], ["Non-Pulsar", "Pulsar"])
     plt.yticks([0, 1], ["Non-Pulsar", "Pulsar"])
-    plt.plot()
+    plt.savefig(file_name)
 
+#testing the MLPC
 x = get_xlsx_file('Dry_Bean_Dataset.xlsx')
 X, y = get_samples(x, 50, ['SIRA', 'DERMASON'])
-accuracy = run_MLPC(X, y, True) 
+accuracy = run_MLPC(X, y, True, 'test') 
 print(accuracy)
